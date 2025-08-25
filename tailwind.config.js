557b1a04175ad3134131534b35e427c2513d0989
Anthony Ahn <anthonyahn@COM-HAHN-24.local>
2025-08-24 19:11:20 -0700

    Fix: Typing lag and implement new header/footer layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./public/**/*.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-beige-bg': '#F5F1E9', // <-- 이 색을 새로 추가했습니다.
+      },
+      backgroundImage: {
+        'custom-background': "url('/public/background.jpg')",
+      },
+    },
   },
   plugins: [],
 }
